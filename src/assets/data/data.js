b2d7d522e728bfe0f52272fe74ce47deb8f4a747
sagnikghosh1999 <saggywebd@gmail.com>
2024-06-23 23:37:16 +0530

    login page initial

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -9,6 +9,11 @@ export const productData = [
       "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344294-3.jpg",
       "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344299-4.jpg",
       "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344305-5.jpg",
+      "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344284-1.jpg",
+      "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344289-2.jpg",
+      "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344294-3.jpg",
+      "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344299-4.jpg",
+      "https://images.bewakoof.com/t1080/gojo-domain-oversized-fit-t-shirt-520746-1658344305-5.jpg",
     ],
     description:
       "Gojo Domain Oversized Fit T-Shirt. Country of Origin - India. Manufactured By - Bewakoof Brands Pvt Ltd, Sairaj Logistic Hub #A5, Bmc Pipeline Road, Opposite All Saints High School, Amne, Bhiwandi,Thane, Maharashtra 421302. Packed By - Bewakoof Brands Pvt Ltd, Sairaj Logistic Hub #A5, Bmc Pipeline Road, Opposite All Saints High School, Amne, Bhiwandi,Thane, Maharashtra 421302. Commodity - Men's T-Shirt.Product Specifications. Oversized fit - Falls loosely on the body. Single Jersey, 100% Cotton Classic, lightweight jersey fabric comprising 100% cotton.",
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import MobileHeader from "../components/header/MobileHeader";
 
 const LoginPage = () => {
   return (
-    <div className="">
+    <div className="h-[100vh]">
       <div className="fixed hidden sm:block top-0 z-50">
         <HeaderTop />
         <HeaderBottom />
@@ -12,8 +12,18 @@ const LoginPage = () => {
       <div className="fixed sm:hidden top-0 z-50">
         <MobileHeader />
       </div>
-      <div className="mt-14 sm:mt-[4.1rem] lg:mt-[4.6rem] sm:px-4 lg:px-5">
-        Login page ...
+      <div className="mt-14 sm:mt-[4.1rem] lg:mt-[4.6rem] ">
+        <div className="hidden md:flex h-[calc(100vh-4.1rem)] fixed bottom-0 left-0 w-full">
+          <div className="flex-1 flex items-center justify-center h-full bg-gradient-to-t from-[#fff4c4] to-[#fff]">
+            <div className="flex flex-col gap-y-16">
+              <div className="text-3xl font-bold ">
+                Welcome to the world of Bewakoof®!
+              </div>
+              <img src="/assets/login.png" alt="login-page-img" />
+            </div>
+          </div>
+          <div className="flex-1">Login form</div>
+        </div>
       </div>
     </div>
   );
